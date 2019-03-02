33ed131f0cba18c36f1feab2717ba4efba62dbe1
宮脇智也 <miyawaki@ootsukashingon-no-MacBook-Pro.local>
2019-03-02 18:53:46 +0900

    技術の部分まで終了

--- sample-app/src/components/member_form.vue
+++ sample-app/src/components/member_form.vue
@@ -8,9 +8,15 @@
       <v-card-title>稼働開始可能日時</v-card-title>
       <v-select solo type="date"></v-select>
       <v-card-title>週に何日稼働できそうですか？</v-card-title>
-      <v-btn outline round class="lime" v-for="day in days" :key="day">{{ day }}</v-btn>
+      <v-btn outline round class="lime" v-for="day in days" :key="`first-${day}`">{{ day }}</v-btn>
       <v-card-title>働き方の条件を教えてください</v-card-title>
       <v-btn outline round class="lime" v-for="work in works" :key="work">{{ work }}</v-btn>
+      <v-card-title>出勤（打ち合わせ）可能エリアを選択してください</v-card-title>
+      <v-btn outline round class="lime" v-for="area in areas" :key="area">{{ area }}</v-btn>
+      <v-card-title>職歴について教えてください</v-card-title>
+      <v-btn outline round class="lime" v-for="job in jobs" :key="`first-${job}`">{{ job }}</v-btn>
+      <v-card-title>実務可能な技術を選択してください</v-card-title>
+      <v-btn round outline class="lime" v-for="skill in skills" :key="skill">{{ skill }}</v-btn>
       <v-textarea solo placeholder="独学で勉強をしてきたので、詰まっても自己解決するのが得意です."></v-textarea>
     </v-card>
   </v-container>
@@ -23,7 +29,10 @@ export default {
     return {
       prices: ['2万~', '2.5万~', '3万~', '3.5万~', '4万~', 'それ以上'],
       days: ['週2', '週3', '週4', '週5',],
-      works: ['常駐', '一部常駐OK', 'フルリモート']
+      works: ['常駐', '一部常駐OK', 'フルリモート'],
+      areas: ['首都圏', '関西', 'その他'],
+      jobs: ['1年未満', '1年', '2年', '3~5年', 'それ以上'],
+      skills: ['JaVa', 'PHP', 'Python', 'C#', 'Ruby', 'React.js', 'Angular.js', 'Vue.js', 'AWS', 'Docker', 'Laravel']
     }
   }
 }
